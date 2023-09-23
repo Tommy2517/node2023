ebfc46d1ec6352ebebf7e058a1c5d17d02f6325e
Tommy2517 <artem07stu@gmail.com>
2023-09-23 21:01:40 +0300

    lesson1

--- app.js
+++ app.js
@@ -1,25 +1,6 @@
 // modules
-const {sayHello} = require('./dir1/dir2/dir3/file');
 const path = require("node:path");
 const fs = require("fs");
-sayHello()
-
-
-// console.log(__dirname)
-// console.log(__filename)
-// console.log(process.cwd())
-
-
-// const pathJoined = path.join(__dirname,'dir1','dir2','dir3','file.js')
-// console.log(pathJoined)
-//
-// const resolve = path.resolve()
-// console.log(resolve);
-
-const dir2 = path.join(__dirname, 'dir1', 'dir2')
-fs.readdir(dir2,(err,arr)=>{
-    console.log(arr)
-})
 
 
 
