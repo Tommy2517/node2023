cbf0d429e5c6a69620a3c728e9359f432b49b41c
Tommy2517 <artem07stu@gmail.com>
2023-09-27 17:39:51 +0300

    install ESLint

--- src/app.ts
+++ src/app.ts
@@ -1,101 +1,99 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 
 import * as fsService from "./fsService";
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/users', async (req:Request, res:Response) => {
+app.get("/users", async (req: Request, res: Response) => {
+  const users = await fsService.reader();
+  res.json(users);
+});
+
+app.post("/users", async (req: Request, res: Response) => {
+  try {
+    const { name, email } = req.body;
+
     const users = await fsService.reader();
-    res.json(users);
+
+    const lastId = users[users.length - 1].id;
+    const newUser = { name, email, id: lastId + 1 };
+    users.push(newUser);
+    await fsService.writer(users);
+    await res.status(201).json(newUser);
+  } catch (e) {
+    await res.status(400).json(e.message);
+  }
 });
 
-app.post('/users', async (req:Request, res:Response) => {
-    try {
-        const {name, email} = req.body;
-
-
-        const users = await fsService.reader();
-
-        const lastId = users[users.length - 1].id;
-        const newUser = {name, email, id: lastId + 1}
-        users.push(newUser);
-        await fsService.writer(users);
-        await res.status(201).json(newUser);
-    } catch (e) {
-        await res.status(400).json(e.message);
+app.get("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const users = await fsService.reader();
+    const user = users.find((user: any) => user.id === Number(id));
+    if (!user) {
+      throw new Error("User not found");
     }
+    await res.json(user);
+  } catch (e) {
+    await res.status(404).json(e.message);
+  }
 });
 
-app.get('/users/:id', async (req:Request, res:Response) => {
-    try {
-        const {id} = req.params;
-        const users = await fsService.reader();
-        const user = users.find((user:any) => user.id === Number(id));
-        if (!user) {
-            throw new Error('User not found');
-        }
-        await res.json(user);
-    } catch (e) {
-        await res.status(404).json(e.message);
+app.delete("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const users = await fsService.reader();
+    const index = users.findIndex((user: any) => user.id === Number(id));
+    if (index === -1) {
+      throw new Error("User not found");
     }
-})
+    users.splice(index, 1);
 
-app.delete('/users/:id', async (req:Request, res:Response) => {
-    try {
-        const {id} = req.params;
+    await fsService.writer(users);
 
-        const users = await fsService.reader();
-        const index = users.findIndex((user:any) => user.id === Number(id));
-        if (index === -1) {
-            throw new Error('User not found');
-        }
-        users.splice(index, 1);
-
-        await fsService.writer(users);
-
-        res.sendStatus(204);
-    } catch (e) {
-        res.status(404).json(e.message);
-    }
+    res.sendStatus(204);
+  } catch (e) {
+    res.status(404).json(e.message);
+  }
 });
 
-app.put('/users/:id', async (req:Request, res:Response) => {
-    try {
-        const {id} = req.params;
-        const {name, email} = req.body;
+app.put("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { name, email } = req.body;
 
-        if (!name || name.length < 2) {
-            throw new Error('Wrong name');
-        }
-        if (!email || !email.includes('@')) {
-            throw new Error('Wrong email');
-        }
-
-        const users = await fsService.reader();
-        const user = users.find((user:any) => user.id === Number(id));
-        if (!user) {
-            throw new Error('User not found');
-        }
-
-        user.email = email;
-        user.name = name;
-
-        await fsService.writer(users);
-
-        res.status(201).json(user);
-    } catch (e) {
-        res.status(404).json(e.message);
+    if (!name || name.length < 2) {
+      throw new Error("Wrong name");
     }
+    if (!email || !email.includes("@")) {
+      throw new Error("Wrong email");
+    }
+
+    const users = await fsService.reader();
+    const user = users.find((user: any) => user.id === Number(id));
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    user.email = email;
+    user.name = name;
+
+    await fsService.writer(users);
+
+    res.status(201).json(user);
+  } catch (e) {
+    res.status(404).json(e.message);
+  }
 });
 
 const PORT = 5001;
 
 app.listen(PORT, () => {
-    console.log(`Server has successfully started on PORT ${PORT}`);
-})
-
+  console.log(`Server has successfully started on PORT ${PORT}`);
+});
 
 // CRUD c - create, r - read, u - update, d - delete
--- src/fsService.ts
+++ src/fsService.ts
@@ -2,18 +2,15 @@ import fs from "node:fs/promises";
 
 import path from "node:path";
 
-const dbPath = path.join(process.cwd(), 'db.json');
+const dbPath = path.join(process.cwd(), "db.json");
 
-const reader = async ():Promise<any[]> => {
-    const json = await fs.readFile(dbPath, {encoding: 'utf-8'});
-    return JSON.parse(json);
-}
+const reader = async (): Promise<any[]> => {
+  const json = await fs.readFile(dbPath, { encoding: "utf-8" });
+  return JSON.parse(json);
+};
 
-const writer = async (users:any[]) => {
-    await fs.writeFile(dbPath, JSON.stringify(users));
-}
+const writer = async (users: any[]) => {
+  await fs.writeFile(dbPath, JSON.stringify(users));
+};
 
-export {
-    reader,
-    writer,
-}
+export { reader, writer };
